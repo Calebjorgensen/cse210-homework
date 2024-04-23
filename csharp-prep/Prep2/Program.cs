54384d78a9ba3a194d31514395e248a6ac1be126
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2024-04-23 10:46:34 -0700

    prep2 is completed'

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string userInput = Console.ReadLine();
+        int gradeNumber = int.Parse(userInput);
+
+        string letter = "";
+
+        if (gradeNumber >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeNumber >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeNumber >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradeNumber >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You grade is {letter}");
+        Console.WriteLine("");
+
+        if (gradeNumber >= 70)
+        {
+            Console.WriteLine($"Great work you earned a {letter}");
+        }
+        else
+        {
+            Console.WriteLine($"You earned a {letter}, please retake");
+        }
+
     }
 }
