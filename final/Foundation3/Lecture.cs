f6656ddb9054fe374fff78052730175e426cf698
Caleb Jorgensen <calebjorgensen@MacBook-Air.local>
2024-06-04 14:00:16 -0700

    Foundation 3 is done

---
+++ final/Foundation3/Lecture.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Diagnostics.Contracts;
+
+public class Lecture : Event
+{
+    private string _speaker;
+    private int _capacity;
+
+    public Lecture(string title, string activity, string date, string time, Address address, string speaker, int capacity) : base(title, activity, date, time, address)
+    {
+        _speaker = speaker;
+        _capacity = capacity;
+    }
+
+    public string GetSpeaker()
+    {
+        return _speaker;
+    }
+
+    public int GetCapacity()
+    {
+        return _capacity;
+    }
+
+    public string ReturnEventType()
+    {
+        return "Lecture";
+    }
+
+    public string GenerateDetailedLecture()
+    {
+        string detailed = GenerateStandard() + "\n";
+        detailed += "Speaker:" + _speaker + "\n";
+        detailed += "Capacity" + _capacity + "pax";
+
+        return detailed;
+    }
+}
--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Console.WriteLine();
+
+        Address address = new Address("832 fake street,", "city", "OR", "USA");
+
+        Lecture lecture = new Lecture("Cars", "PG", "6-10-24", "8PM", address, "Caleb", 200);
+
+        Console.WriteLine("Event 1");
+
+        Console.WriteLine();
+        Console.WriteLine(lecture.GenerateStandard());
+        Console.WriteLine();
+        Console.WriteLine(lecture.GenerateDetailedLecture());
+        Console.WriteLine();
+
+        Address address1 = new Address("A-1 warehouse", "743 not his lane", "CA", "USA");
+        Reception reception = new Reception("Boogie Dance", "Bring your dancing shoes", "8am -", "12pm", address, "The Johnsons");
+
+        Console.WriteLine("Event 2");
+        Console.WriteLine();
+        Console.WriteLine(reception.GenerateStandard());
+        Console.WriteLine();
+        Console.WriteLine(reception.GenerateDetailReception());
     }
 }
