9d40ad65faeaa689056f58bf860624cb77e47dc6
Caleb Jorgensen <calebjorgensen@MacBook-Air.lan>
2024-06-04 18:34:19 -0700

    Foundation 4 is done

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        Console.WriteLine();
+
+        Console.WriteLine("Running");
+        Running running = new Running("06 June 2024", 30, 5);
+        Console.WriteLine(running.GetSummary());
+
+        Console.WriteLine();
+
+        Console.WriteLine("Cycling");
+        Bicycles bicycles = new Bicycles("04 June 2024", 45, 10);
+        Console.WriteLine(bicycles.GetSummary());
+
+        Console.WriteLine();
+
+        Console.WriteLine("Swimming");
+        Swimming swimming = new Swimming("02 June 2024", 20, 20);
+        Console.WriteLine(swimming.GetSummary());
+
+        Console.WriteLine();
     }
 }
